d0cf3bf41937b534c366482f553c3e725cfe4111
minh-trung-web <tvmt.trung2006@gmail.com>
2026-09-21 14:03:27 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Check username and password
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "9999") {
         return true;
     }
 
