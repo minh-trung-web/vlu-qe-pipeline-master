68ce7c5d499744c4e318ba9b8146849d20be0cbb
minh-trung-web <tvmt.trung2006@gmail.com>
2026-09-21 14:15:38 +0700

    Fix login logic

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Check username and password
-    if (username === "admin" && password === "9999") {
+    if (username === "admin" && password === "123") {
         return true;
     }
 
